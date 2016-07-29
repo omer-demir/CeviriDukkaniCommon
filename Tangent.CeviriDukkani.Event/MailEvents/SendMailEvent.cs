4e1d3f3fb35fba19eeab6a41558fd0e743c05c2a
ondercalbay <ondercalbay@hotmail.com>
2016-07-29 22:37:31 +0300

    MailEvent

---
+++ Tangent.CeviriDukkani.Event/MailEvents/SendMailEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Tangent.CeviriDukkani.Domain.Common;
+using Tangent.CeviriDukkani.Domain.Dto.Enums;
+using Tangent.CeviriDukkani.Domain.Dto.Translation;
+
+namespace Tangent.CeviriDukkani.Event.MailEvents
+{
+    public class SendMailEvent: IEvent
+    {
+        #region Implementation of IEvent
+
+        public Guid Id { get; set; }
+        public int CreatedBy { get; set; }
+
+        #endregion
+        public MailSenderTypeEnum MailSender { get; set; }
+        public string Message { get; set; }
+        public string Subject { get; set; }
+        public List<string> To { get; set; }
+    }
+}
