a197bdc28a1d61ba12530bd1011e5835c650696b
omer-demir <farukomer.demir@hepsiburada.com>
2016-07-30 17:09:25 +0300

    Changes

---
+++ Tangent.CeviriDukkani.Domain/Dto/Common/TerminologyPriceRateDto.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Tangent.CeviriDukkani.Domain.Dto.Common {
+    public class TerminologyPriceRateDto:BaseDto {
+        public int TerminologyId { get; set; }
+        public virtual TerminologyDto Terminology { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
--- Tangent.CeviriDukkani.WebCore/BaseControllers/BaseApiController.cs
+++ Tangent.CeviriDukkani.WebCore/BaseControllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Web.Http;
@@ -43,6 +44,24 @@ namespace Tangent.CeviriDukkani.WebCore.BaseControllers {
                 Content = new ObjectContent(data.GetType(), data, Formatter)
             };
             return response;
+        }
+
+        public int GetUserIdFromHeader() {
+            var values = Request.Headers.GetValues("x-cev-userid").ToList();
+            if (!values.Any()) {
+                return default(int);
+            }
+
+            return int.Parse(values.FirstOrDefault());
+        }
+
+        public string GetUserNameFromHeader() {
+            var values = Request.Headers.GetValues("x-cev-username").ToList();
+            if (!values.Any()) {
+                return default(string);
+            }
+
+            return values.FirstOrDefault();
         }
     }
 }
