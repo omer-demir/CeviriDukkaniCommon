116518d2cc7fb9e9f53b3508e802570486e54e45
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-03 23:50:16 +0300

    changes

---
+++ Tangent.CeviriDukkani.Domain/Dto/Request/UpdateDocumentPartContentRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+    public class UpdateDocumentPartContentRequestDto {
+        public int ChangerId { get; set; }
+        public int TranslationDocumentPartId { get; set; }
+        public string Content { get; set; }
+    }
+}
