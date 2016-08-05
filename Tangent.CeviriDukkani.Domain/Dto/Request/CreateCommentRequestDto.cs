72f87ddd93730d8f266c35d504139a1121e23345
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-06 02:14:59 +0300

    changes

---
+++ Tangent.CeviriDukkani.Domain/Dto/Request/CreateCommentRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+    public class CreateCommentRequestDto {
+        public int TranslationDocumentPartId { get; set; }
+        public int CommentCreatorId { get; set; }
+        public string Content { get; set; }
+    }
+}
