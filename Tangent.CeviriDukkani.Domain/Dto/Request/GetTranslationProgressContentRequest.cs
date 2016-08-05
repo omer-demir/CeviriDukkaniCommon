1a0d4b59590dd2cc3402bc5ad35d127c98736ed2
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-05 19:21:05 +0300

    changes

---
+++ Tangent.CeviriDukkani.Domain/Dto/Request/GetTranslationProgressContentRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+    public class GetTranslationProgressContentRequest {
+        public int TranslationDocumentPartId { get; set; }
+        public int UserId { get; set; }
+    }
+}
