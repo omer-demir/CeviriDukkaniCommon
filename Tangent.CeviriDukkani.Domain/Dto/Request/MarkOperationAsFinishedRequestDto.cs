34bacf9424c8e01dae674df31d2483844260fae6
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-06 02:17:22 +0300

    changes

---
+++ Tangent.CeviriDukkani.Domain/Dto/Request/MarkOperationAsFinishedRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+    public class MarkOperationAsFinishedRequestDto {
+        public int TranslationDocumentPartId { get; set; }
+        public int UserId { get; set; }
+    }
+}
