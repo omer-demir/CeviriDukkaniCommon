6e517147cda0abafedb8ee3d988616c0eff52e62
emircokdogan <emircokdogan@gmail.com>
2016-08-06 17:22:57 +0300

    document id added

--- Tangent.CeviriDukkani.Domain/Dto/Audit/DocumentAuditDto.cs
+++ Tangent.CeviriDukkani.Domain/Dto/Audit/DocumentAuditDto.cs
@@ -6,6 +6,7 @@ namespace Tangent.CeviriDukkani.Domain.Dto.Audit
     {
         public int Id { get; set; }
 
+        public int DocumentId { get; set; }
         public string Message { get; set; }
         public string Status { get; set; }
         public DateTime Date { get; set; }
--- Tangent.CeviriDukkani.Domain/Entities/Audit/DocumentAudit.cs
+++ Tangent.CeviriDukkani.Domain/Entities/Audit/DocumentAudit.cs
@@ -6,6 +6,7 @@ namespace Tangent.CeviriDukkani.Domain.Entities.Audit
     {
         public int Id { get; set; }
         
+        public int DocumentId { get; set; }
         public string Message { get; set; }
         public string Status { get; set; }
         public DateTime Date { get; set; }
