009eeb4532b4a99758a21818797cc4b23f4b7f38
emircokdogan <emircokdogan@gmail.com>
2016-08-11 02:00:34 +0300

    generate char count

--- Tangent.CeviriDukkani.Domain/Common/StringParser.cs
+++ Tangent.CeviriDukkani.Domain/Common/StringParser.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Tangent.CeviriDukkani.Domain.Common {
-    public class StringParser {
+namespace Tangent.CeviriDukkani.Domain.Common
+{
+    public class StringParser
+    {
         private string SourceText { get; set; }
 
-        public StringParser(string sourceText) {
+        public StringParser(string sourceText)
+        {
             this.SourceText = sourceText;
         }
 
-        private List<string> SplitSentences() {
+        private List<string> SplitSentences()
+        {
             // create a local string variable
             // set to contain the string passed it
             string sTemp = SourceText;
@@ -27,7 +29,8 @@ namespace Tangent.CeviriDukkani.Domain.Common {
                 Regex.Split(sTemp, @"(?<=['""A-Za-z0-9][\.\!\?])\s+(?=[A-Z])");
 
             // loop the sentences
-            for (int i = 0; i < splitSentences.Length; i++) {
+            for (int i = 0; i < splitSentences.Length; i++)
+            {
                 // clean up the sentence one more time, trim it,
                 // and add it to the array list
                 string sSingleSentence =
@@ -39,108 +42,20 @@ namespace Tangent.CeviriDukkani.Domain.Common {
             // all sentences added
             return al;
         }
+        
+        public int GenerateCharacterCount(bool withoutWhitespaces = false)
+        {
+            var regexForReplace = withoutWhitespaces
+                                    ? @"[^A-Za-z0-9]"
+                                    : @"[^A-Za-z0-9\s]";
+            string sxTemp = SourceText.Replace(Environment.NewLine, string.Empty);
 
-        private List<string> ReasonableParser() {
-            List<string> al = new List<string>();
-
-            // get a string from the contents of a textbox
-            string sTemp = SourceText;
-            sTemp = sTemp.Replace(Environment.NewLine, " ");
-
-            // split the string using sentence terminations
-            char[] arrSplitChars = { '.', '?', '!' };  // things that end a sentence
-
-            //do the split
-            string[] splitSentences = sTemp.Split(arrSplitChars, StringSplitOptions.RemoveEmptyEntries);
-
-            // loop the array of splitSentences
-            for (int i = 0; i < splitSentences.Length; i++) {
-                // find the position of each sentence in the
-                // original paragraph and get its termination ('.', '?', '!') 
-                int pos = sTemp.IndexOf(splitSentences[i]);
-                char[] arrChars = sTemp.Trim().ToCharArray();
-                char c = arrChars[pos + splitSentences[i].Length];
-
-                // since this approach looks only for the first instance
-                // of the string, it does not handle duplicate sentences
-                // with different terminations.  You could expand this
-                // approach to search for later instances of the same
-                // string to get the proper termination but the previous
-                // method of using the regular expression to split the
-                // string is reliable and less bothersome.
-
-                // add the sentences termination to the end of the sentence
-                al.Add(splitSentences[i].ToString().Trim() + c.ToString());
-            }
-
-            return al;
+            var clearedText = Regex.Replace(sxTemp, regexForReplace, string.Empty);
+            return clearedText.Length;
         }
 
-        private List<string> IDontCareHowItEndsParser() {
-            string sTemp = SourceText;
-            sTemp = sTemp.Replace(Environment.NewLine, " ");
-
-            // split the string using sentence terminations
-            char[] arrSplitChars = { '.', '?', '!' };  // things that end a sentence
-
-            //do the split
-            string[] splitSentences = sTemp.Split(arrSplitChars, StringSplitOptions.RemoveEmptyEntries);
-
-            List<string> al = new List<string>();
-            for (int i = 0; i < splitSentences.Length; i++) {
-                splitSentences[i] = splitSentences[i].ToString().Trim();
-                al.Add(splitSentences[i]);
-            }
-
-            return al;
-        }
-
-        public int GenerateCharacterCount(bool withoutWhitespaces = false) {
-            int rtn = 0;
-
-            // clean up the string by
-            // removing newlines and by trimming
-            // both ends
-            string sTemp = SourceText;
-            sTemp = sTemp.Replace(Environment.NewLine, string.Empty);
-            sTemp = sTemp.Trim();
-
-            // split the string into sentences 
-            // using a regular expression
-            string[] splitSentences =
-                Regex.Split(sTemp,
-                    @"(?<=['""A-Za-z0-9][\.\!\?])\s+(?=[A-Z])");
-
-            // loop through the sentences to get character counts
-            for (int cnt = 0; cnt < splitSentences.Length; cnt++) {
-                // get the current sentence
-                string sSentence = splitSentences[cnt].ToString();
-
-                // trim it
-                sSentence = sSentence.Trim();
-
-                // convert it to a character array
-                char[] sentence = sSentence.ToCharArray();
-
-                // test each character and
-                // add it to the return value
-                // if it passes
-                for (int i = 0; i < sentence.Length; i++) {
-                    // make sure it is a letter, number,
-                    // punctuation or whitespace before
-                    // adding it to the tally
-                    if (char.IsLetterOrDigit(sentence[i]) ||
-                        char.IsPunctuation(sentence[i]) ||
-                        (withoutWhitespaces || char.IsWhiteSpace(sentence[i])))
-                        rtn += 1;
-                }
-            }
-
-            // return the final tally
-            return rtn;
-        }
-
-        public int GenerateWordCount(Func<List<string>> action) {
+        public int GenerateWordCount(Func<List<string>> action)
+        {
             // declare a return value
             int rtn = 0;
 
@@ -148,7 +63,8 @@ namespace Tangent.CeviriDukkani.Domain.Common {
 
             // iterate through the entire list
             // of sentences
-            foreach (string sSentence in sentences) {
+            foreach (string sSentence in sentences)
+            {
                 // define an empty space as the split
                 // character
                 char[] arrSplitChars = { ' ' };
@@ -166,7 +82,8 @@ namespace Tangent.CeviriDukkani.Domain.Common {
             return rtn;
         }
 
-        public int GenerateSentenceCount(Func<List<string>> action) {
+        public int GenerateSentenceCount(Func<List<string>> action)
+        {
             // create a return value
             int rtn = 0;
 
@@ -179,7 +96,8 @@ namespace Tangent.CeviriDukkani.Domain.Common {
             return rtn;
         }
 
-        public List<string> SplitByCount(int count) {
+        public List<string> SplitByCount(int count)
+        {
             var charCount = GenerateCharacterCount();
             var sentences = SplitSentences();
             var avgCharCountByPart = charCount / count;
@@ -188,10 +106,12 @@ namespace Tangent.CeviriDukkani.Domain.Common {
 
             var sum = 0;
             var lastSentenceCount = 0;
-            for (int j = 0; j < sentences.Count; j++) {
+            for (int j = 0; j < sentences.Count; j++)
+            {
                 sum += sentences[j].Length;
 
-                if (sum >= avgCharCountByPart) {
+                if (sum >= avgCharCountByPart)
+                {
                     result.Add(string.Join(" ", sentences.Skip(lastSentenceCount).Take(j - lastSentenceCount)));
                     lastSentenceCount = j;
                     sum = 0;
