e52288bc0abbdbf4d38c9ac936af8c5a7d0a9c2a
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-25 00:21:56 +0300

    changes

--- Tangent.CeviriDukkani.Domain/Dto/Request/AcceptOfferRequestDto.cs
+++ Tangent.CeviriDukkani.Domain/Dto/Request/AcceptOfferRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+﻿using Tangent.CeviriDukkani.Domain.Dto.Enums;
+
+namespace Tangent.CeviriDukkani.Domain.Dto.Request {
     public class AcceptOfferRequestDto {
         public int BidderId { get; set; }
+        public UserRoleTypeEnum UserRoleType { get; set; }
         public int OrderDetailId { get; set; }
         public decimal? Price { get; set; }
+
     }
 }
