384e24ad5d4d9ba5091851c7353c291ce1fe9098
omer-demir <farukomer.demir@hepsiburada.com>
2016-10-04 00:03:07 +0300

    #000 omer new request dto added

---
+++ Tangent.CeviriDukkani.Domain/Dto/Request/UpdateUserStepRequestDto.cs
@@ -0,0 +1,8 @@
+﻿using Tangent.CeviriDukkani.Domain.Dto.System;
+
+namespace Tangent.CeviriDukkani.Domain.Dto.Request {
+    public class UpdateUserStepRequestDto {
+        public UserDto User { get; set; }
+        public int Step { get; set; }
+    }
+}
